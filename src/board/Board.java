cd4416f42276883c4c14f6374106c163b0c49763
abdullah.mahzuddin <abdullah.mahzuddin@le-campus-numerique.fr>
2022-11-29 11:57:58 +0100

    Eteration  5 fini aletoire

--- src/board/Board.java
+++ src/board/Board.java
@@ -1,43 +1,87 @@
 package board;
 
-import EquipementOfensif.*;
+import EquipementOfensif.BouclesDeFeu;
+import EquipementOfensif.Eclairs;
+import EquipementOfensif.Epees;
+import EquipementOfensif.Massues;
 import Personage.Personnage;
 import ennemy.Dragon;
-
 import ennemy.Goblines;
 import ennemy.Sorcier;
 import potion.GrandesPotion;
-import potion.Potion;
 import potion.PotionStandard;
-
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Board {
-    ArrayList<Case> bord = new ArrayList<>();
+    ArrayList<Case> board = new ArrayList<>();
 
     private final int nbcase;
 
     ////////////////  Constructor   ///////////////////
     public Board() {
-        this.nbcase=64;
-        for (int i = 0; i<nbcase; i++){
-            switch (i){
-                case 45,52,56,62 ->bord.add(new Dragon("Dragon",10));
-                case 10, 20, 25, 32, 35, 36, 37, 40, 44 , 47 ->bord.add(new Sorcier("Sorcier",15));
-                case 3, 6, 9 , 12, 15, 18, 21, 24, 27 , 30 -> bord.add(new Goblines("Gobline",20));
-                case 2, 11, 5, 22, 38 -> bord.add(new Massues("Massue"));
-                case 19, 26, 42 , 53->bord.add(new Epees("Epee"));
-                case 1, 4 , 8, 17 , 23 -> bord.add(new Eclairs("éclairs")) ;
-                case 48, 49 -> bord.add(new BouclesDeFeu("boulde foue"));
-                case 7, 13, 31, 33, 39, 43 -> bord.add(new PotionStandard("PotionStandard",3));
-                case 28, 41 -> bord.add(new GrandesPotion("GrandPotion",5));
-                default -> bord.add(new CaseVide("Case Vide"));
+        this.nbcase = 64;
+        ArrayList<Integer> indexesCasesVides = new ArrayList<>();
+
+        // ajout de cases vides dans board
+        // initialisation de la list caseVides des index des cases vides
+        for (int i = 0; i < nbcase; i++) {
+            board.add(new CaseVide("this is my empty Box"));
+            indexesCasesVides.add(i);
+        }
+
+        Map<String, Integer> dic = new HashMap<>();
+        dic.put("Dragon", 4);
+        dic.put("Sorcier", 10);
+        dic.put("Goblines", 10);
+        dic.put("Massues", 5);
+        dic.put("Epee", 4);
+        dic.put("Eclairs", 5);
+        dic.put("BoulesDeFeu", 2);
+        dic.put("PotionStandard", 6);
+        dic.put("GrandesPotion", 2);
+
+
+        for (String key : dic.keySet()) {
+            int items = dic.get(key);
+            while (items > 0) {
+                int x = (int) (Math.random() * (indexesCasesVides.size()-1));
+                int c = indexesCasesVides.get(x);
+                instaciateAndAssociate(c,key);
+                indexesCasesVides.remove(x);
+                items--;
             }
         }
     }
 
-    ////////////////  getter & setter   ///////////////////
+    public void instaciateAndAssociate(int c, String player) {
 
+        if (player == "Dragon") {
+            board.set(c, new Dragon("Dragon", 10));
+        } else if (player == "Sorcier") {
+            board.set(c, new Sorcier("Sorcier", 15));
+        } else if (player == "Goblines") {
+            board.set(c, new Goblines("Gobline", 20));
+        } else if (player == "Massues") {
+            board.set(c, new Massues("Massue"));
+        } else if (player == "Epee") {
+            board.set(c, new Epees("Epee"));
+        } else if (player == "Eclairs") {
+            board.set(c, new Eclairs("éclairs"));
+        } else if (player == "BoulesDeFeu") {
+            board.set(c, new BouclesDeFeu("boulde foue"));
+        } else if (player == "PotionStandard") {
+            board.set(c, new PotionStandard("PotionStandard", 3));
+        } else if (player == "GrandesPotion") {
+            board.set(c, new GrandesPotion("GrandPotion", 5));
+        }else {
+            board.set(c,new CaseVide("I am Empty Box") );
+        }
+    }
+
+
+    ////////////////  getter & setter   ///////////////////
 
 
     public int getNbcase() {
@@ -45,16 +89,16 @@ public class Board {
     }
 
     public ArrayList<Case> getBord() {
-        return bord;
+        return board;
     }
 
     public void setBord(ArrayList<Case> bord) {
-        this.bord = bord;
+        this.board = bord;
     }
 
 
     public void displayBoard(Personnage personage) {
-        for (int i = 0; i < bord.size(); i++) {
+        for (int i = 0; i < board.size(); i++) {
             if (personage.getPosition() == i) {
                 System.out.print("**|");
             } else {
